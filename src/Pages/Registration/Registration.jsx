c347f6ba8461719da18a28103612a1aee90ca651
Md Nirab <nabilnirab255@gmail.com>
2024-03-17 23:57:04 +0600

    login page design update

--- src/Pages/Registration/Registration.jsx
+++
@@ -1,9 +0,0 @@
-const Registration = () => {
-    return (
-        <div>
-            <h2>Wellcome To Forex  Market</h2>
-        </div>
-    );
-};
-
-export default Registration;
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,17 +1,28 @@
 import {createBrowserRouter} from "react-router-dom";
 import Main from "../layouts/Main/Main";
 import Home from "../Pages/Home/Home";
+import Errorpage from "../Pages/Errorpage/Errorpage";
+import Login from "../Pages/Login/Login";
+import Register from "../Pages/Register/Register";
 
   
 export  const router = createBrowserRouter([
     {
       path: "/",
       element: <Main></Main>,
-
+      errorElement: <Errorpage></Errorpage>,
       children: [
         {
         path: "/",
         element: <Home></Home>  
+        },
+        {
+          path: "/login",
+          element: <Login></Login>
+        },
+        {
+          path: "/register",
+          element: <Register></Register>
         }
       ]
     }
