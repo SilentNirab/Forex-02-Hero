7c3dadc3abb142bb8bd539a1119bf18741dfb6b0
FirozTheProgrammer <firozhasan1542@gmail.com>
2024-03-20 23:15:30 +0600

    some bug fix and added footer section

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import { NavLink } from "react-router-dom";
 import Container from "../../../Components/Container/Container";
+import logo from "../../../assets/images/logo-transparent-CROP.png"
 
 const Navbar = () => {
   const NavNavLinks = (
@@ -9,7 +10,7 @@ const Navbar = () => {
           to={"/"}
           className={({ isActive, isPending }) =>
             isActive
-              ? "border-green-500 border-b-2 px-1 py-1"
+              ? "border-green-500 border-b-2  py-1"
               : isPending
               ? "pending "
               : ""
@@ -23,7 +24,7 @@ const Navbar = () => {
           to={"/blogs"}
           className={({ isActive, isPending }) =>
             isActive
-              ? "border-green-500 border-b-2 px-1 py-2"
+              ? "border-green-500 border-b-2  py-1"
               : isPending
               ? "pending "
               : ""
@@ -37,7 +38,7 @@ const Navbar = () => {
           to={"/aboutUs"}
           className={({ isActive, isPending }) =>
             isActive
-              ? "border-green-500 border-b-2 px-1 py-2"
+              ? "border-green-500 border-b-2  py-1"
               : isPending
               ? "pending "
               : ""
@@ -49,55 +50,56 @@ const Navbar = () => {
     </>
   );
   return (
-    <Container>
-      <div className="navbar bg-base-100 ">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <label tabIndex={0} className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
+    <div className="border-b border-gray">
+      <Container>
+        <div className="navbar bg-base-100 ">
+          <div className="navbar-start">
+            <div className="dropdown">
+              <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />
+                </svg>
+              </label>
+              <ul
+                tabIndex={0}
+                className=" menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
-            </label>
-            <ul
-              tabIndex={0}
-              className=" menu-sm dropdown-content  mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-            >
-              {NavNavLinks}
-            </ul>
+                {NavNavLinks}
+              </ul>
+            </div>
+            <NavLink to={"/"}>
+            <img className="max-h-16" src={logo} alt="" />
+            </NavLink>
           </div>
-          <NavLink to={"/"}>
-            <p>Logo</p>
-          </NavLink>
+          <div className="navbar-center hidden lg:flex">
+            <ul className=" menu-horizontal px-1 space-x-4">{NavNavLinks}</ul>
+          </div>
+          <div className="navbar-end gap-x-2">
+            <NavLink to={"/login"}>
+              <button className="transition ease-in-out  px-4 py-2 text-black rounded-md font-medium hover:bg-black hover:text-white duration-500">
+                Login
+              </button>
+            </NavLink>
+            <NavLink to={"/register"}>
+              <button className="transition ease-in-out px-4 py-2 bg-green-500 text-white rounded-md font-medium hover:bg-green-600 duration-500">
+                Register Now
+              </button>
+            </NavLink>
+          </div>
         </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className=" menu-horizontal px-1 space-x-4">{NavNavLinks}</ul>
-        </div>
-        <div className="navbar-end gap-x-2">
-          <NavLink to={"/login"}>
-            <button className="transition ease-in-out  px-4 py-2 text-black rounded-md font-medium hover:bg-black hover:text-white duration-500">
-              Login
-            </button>
-          </NavLink>
-          <NavLink to={"/register"}>
-            <button className="transition ease-in-out px-4 py-2 bg-green-500 text-white rounded-md font-medium hover:bg-green-600 duration-500">
-              Register Now
-            </button>
-          </NavLink>
-   
-        </div>
-      </div>
-    </Container>
+      </Container>
+    </div>
   );
 };
 
