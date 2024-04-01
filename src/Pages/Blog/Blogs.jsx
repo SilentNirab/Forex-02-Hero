f5b669ec5f9c5e0f9676bd67de82cb290d3e1026
FirozTheProgrammer <firozhasan1542@gmail.com>
2024-04-01 20:14:50 +0600

    fixed bug

--- src/Pages/Blog/Blogs.jsx
+++ src/Pages/Blog/Blogs.jsx
@@ -15,7 +15,7 @@ const Blogs = () => {
       <Container>
         <div>
           <div className="text-center">
-            <h2 className="text-3xl md:text-5xl inline border-b-2 border-green-600  font-semibold">
+            <h2 className="text-3xl md:text-6xl inline border-b-2 border-green-600  font-semibold">
               Our Team
             </h2>
             <p className="md:max-w-[600px] mx-auto py-5">
